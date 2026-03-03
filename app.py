085d82ee2c60f9bdd7434a1c8d4e25c0c338a1ca
William <billy_maguire@hotmail.co.uk>
2026-03-03 14:06:37 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 from flask import Flask, render_template, request, redirect, url_for, session, flash
 
 app = Flask(__name__)
-app.secret_key = "changeme-secret-key"
+app.secret_key = "GreenWall-secret-key"
 
-APP_TITLE = "ChangeMe"
+APP_TITLE = "GreenWall"
 
 # Fake product data
 products = [
